ad393d6984d4d42287c10e2905448f9edcbc7e0d
Blin113 <54438172+Blin113@users.noreply.github.com>
2020-10-07 09:21:58 +0200

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Player
+    {
+        Texture2D man;
+        Vector2 manPos = new Vector2(200, 20);
+
+        public Player(Texture2D man)
+        {
+            this.man = man;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kState = Keyboard.GetState();
+
+
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            SpriteBatch.Draw(man, new Rectangle((int)manPos.X, (int)manPos.Y, 20, 20), Color.White);
+        }
+    }
+}
